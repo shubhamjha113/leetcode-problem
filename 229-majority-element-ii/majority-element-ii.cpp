ee1ccbd33f0beb275a868258512a6ff8ecbdf32c
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2024-12-28 19:50:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (58.05%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -3,15 +3,45 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int> ans;
         int n = nums.size();
-         map<int , int> count;
-         for(int i=0;i<n;i++){
-            count[nums[i]]++;
-         }
-         for(auto j : count){
-            if(j.second > n/3){
-                ans.push_back(j.first);
+        if(n<2) return nums;
+        int first = nums[0],second= nums[0] , fc= 0, sc=0;  // fc- first count   sc- second count
+        for(int i=0;i<n;i++){
+            if( first == nums[i]){ 
+                first = nums[i];
+                fc++;
             }
-         } 
-        return ans;
+            else if ( second == nums[i]){
+                second = nums[i];
+                sc++;
+            }
+            else if(fc == 0){
+                first = nums[i];
+                fc=1;
+            }
+            else if(sc == 0){
+                 second  = nums[i];
+                sc=1;
+            }
+            else {
+                fc--;
+                sc--;
+            }
+        }
+        if(first == second){
+            ans.push_back(first);
+            return ans;
+        }
+        fc=0,sc=0;
+        for(int i=0;i<n;i++){
+            if(first == nums[i]) fc++;
+            if(second == nums[i]) sc++;
+        }
+        if(fc > n/3){
+            ans.push_back(first);
+        }
+        if(sc > n/3){
+            ans.push_back(second);
+        }
+        return ans;  
     }
 };
