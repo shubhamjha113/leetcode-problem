46afe3f427d98a726fdeca36507875981f87a594
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-01-10 00:42:49 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (49.15%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,45 +1,28 @@
 class Solution {
 public:
-    int findMin(vector<int>& nums) {
-       int s=0,e=nums.size()-1;
-       int mid = s + (e-s)/2;
-       while(s<e){
-        if(nums[mid] > nums[e]){
-            s=mid+1;
-        }
-        else{
-            e=mid;
-        }
-        mid = s + (e-s)/2;
-       }
-       return s;
-    }
-    int binarySearch(vector<int>& nums,int s,int e, int target) {
-        int n=nums.size();
-        
-        int mid = s + ((e-s)/2);
-        while(s<=e){
-            if(nums[mid] == target){
-                return mid;
-            }
-            else if(target>nums[mid]){
-                s=mid+1;
-            }
-            else{
-                e=mid-1;
-            }
-            mid = s + ((e-s)/2);
-        }
-        return -1;
-    }
     int search(vector<int>& nums, int target) {
         int n = nums.size();
-        int pivot = findMin(nums);
-        if(target >= nums[pivot] && target <= nums[n-1]){
-            return binarySearch(nums,pivot,n-1,target);
+        int s=0,e=n-1;
+        while(s<=e){
+            int mid = (s+e)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[s] <= nums[mid]){
+                if(nums[s] <= target && target<=nums[mid]){
+                    e=mid-1;
+                }
+                else{
+                    s=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[e]){
+                    s=mid+1;
+                }
+                else{
+                    e=mid-1;
+                }
+            }
         }
-        else{
-            return binarySearch(nums,0,pivot-1,target);
-        }
+        return -1;
     }
 };
