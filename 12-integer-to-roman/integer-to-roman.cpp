74d60c4efe232c33018f12796c7536f40d85ab6b
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-02-12 21:22:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (67.25%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -3,9 +3,9 @@ public:
     string intToRoman(int num) {
         string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
         string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
-        string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
-        string ths[]={"","M","MM","MMM"};
+        string hundreds[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        string thousands[]={"","M","MM","MMM"};
         
-        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+        return thousands[num/1000] + hundreds[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
     }
 };
