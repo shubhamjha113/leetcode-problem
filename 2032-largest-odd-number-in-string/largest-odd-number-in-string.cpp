c311cb07b2d1abf0c7fd824910e42974f4d499e5
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-02-16 15:32:27 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (89.88%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -4,18 +4,18 @@ public:
         string ans = "";
         int index =-1;
         int n = num.size();
-        
+        if(num[n-1] %2 == 1){
+            return num;
+        }
         for(int i=n-1;i>=0;i--){
-            int ch = num[i] - '0';
-            if(ch%2==1){
+            // int ch = num[i] - '0';
+            if(num[i]%2==1){
                 index = i;
                 break;
             }
         }
-        //ans = num.substr(0,index+1);          // extract from num
-        for(int i=0;i<index+1;i++){
-            ans += num[i];
-        }
+         ans = num.substr(0,index+1);          // extract from num
+        
         
      return ans; 
     }
