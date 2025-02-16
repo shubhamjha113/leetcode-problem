378970814dc11395781ed1095597c3102df98754
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-02-16 15:28:26 +0530

    Time: 16 ms (5.39%) | Memory: 19.3 MB (5.42%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -3,13 +3,16 @@ public:
     string largestOddNumber(string num) {
         string ans = "";
         int index =-1;
-        for(int i=0;i<num.size();i++){
+        int n = num.size();
+        
+        for(int i=n-1;i>=0;i--){
             int ch = num[i] - '0';
             if(ch%2==1){
                 index = i;
+                break;
             }
         }
-        //ans = num.substr(0,index+1);
+        //ans = num.substr(0,index+1);          // extract from num
         for(int i=0;i<index+1;i++){
             ans += num[i];
         }
