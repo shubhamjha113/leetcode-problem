f1c5402d3326e5f8a3306a0de8ff08db5a51c89f
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-02-17 15:30:43 +0530

    Time: 260 ms (12.69%) | Memory: 29 MB (21.20%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -4,17 +4,15 @@ public:
         int res = 0;
         
         for (int i = 0; i < s.size(); i++) {
-            vector<int> count(26,0);
+            map<int,int> count;
             for (int j = i; j < s.size(); j++) {
                 count[s[j]-'a']++; 
                     int maxfreq = 0;
                     int minfreq = INT_MAX;
                     
-                    for (int k=0;k<26;k++) {
-                        if(count[k]>0){
-                            maxfreq = max(maxfreq, count[k]);
-                            minfreq = min(minfreq, count[k]);
-                        }
+                    for (auto k :count) {
+                            maxfreq = max(maxfreq, k.second);
+                            minfreq = min(minfreq, k.second);
                     }
                     
                     res += (maxfreq - minfreq);
