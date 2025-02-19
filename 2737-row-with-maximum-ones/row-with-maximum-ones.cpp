73be79f1ae8d4ec782b5766cd5338b042a1b22ee
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-02-20 01:19:51 +0530

    Time: 9 ms (14.82%) | Memory: 64.5 MB (89.54%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
@@ -5,7 +5,7 @@ public:
         int row = mat.size();
         int col  = mat[0].size();
         int oneCount = INT_MIN;
-        int rowNo = -1;
+        int rowNo = 0;
         for(int i=0;i<row;i++){
             int count =0;
             for(int j=0;j<col;j++){
