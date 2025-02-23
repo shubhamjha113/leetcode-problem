a6933e1761b6078766c09371bfd9d4e45cb2048b
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-02-23 13:21:23 +0530

    Time: 3 ms (70.61%) | Memory: 16.4 MB (18.51%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+void solve(int n , vector<string> &ans,string output,int L,int R)
+{
+    //base case
+    if(L+R == 2*n){
+        ans.push_back(output);
+        return;
+    }
+
+    //add left
+    if(L<n){
+        output.push_back('(');
+        solve(n,ans,output,L+1,R);
+        output.pop_back();
+    }
+    if(R<L){
+       output.push_back(')');
+        solve(n,ans,output,L,R+1);
+        output.pop_back();
+    } 
+    
+}
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string output = "";
+        solve(n,ans,output,0,0);
+        return ans;
+    }
+};
