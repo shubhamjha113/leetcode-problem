1d2cf7468826a86aacd396b7df54000e97a1f00c
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-02-25 22:35:50 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (69.41%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,20 +1,33 @@
 class Solution {
 private:
 
-void solve(vector<int>& nums,vector<vector<int>> &ans,vector<int> &temp,int index)
+void solve(vector<int>& nums,vector<vector<int>> &ans,vector<int> &temp,int i)
 {
-    // if(index>=nums.size()){
-    //     ans.push_back(temp);
-    //     return;
-    // }
-    ans.push_back(temp);
-     for(int i=index;i<nums.size();i++){
-       if(i != index && nums[i] == nums[i-1]) continue;
+    if(i == nums.size() ){
+       ans.push_back(temp);
+       return; 
+    }
+    else{
+        temp.push_back(nums[i]);
+        solve(nums,ans,temp,i+1);
+        temp.pop_back();
 
-       temp.push_back(nums[i]);
-       solve(nums,ans,temp,i+1);
-       temp.pop_back();
-     }
+        while(i+1 < nums.size() && nums[i] == nums[i+1]) i++;
+        solve(nums,ans,temp,i+1);
+
+    }
+
+
+
+
+    // ans.push_back(temp);
+    //  for(int i=index;i<nums.size();i++){
+    //    if(i != index && nums[i] == nums[i-1]) continue;
+
+    //    temp.push_back(nums[i]);
+    //    solve(nums,ans,temp,i+1);
+    //    temp.pop_back();
+    //  }
 
 }
 public:
