ac41f1e89dd3803a34476a9a9f392450c2ba1544
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-02-25 13:01:22 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (61.40%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,7 +1,6 @@
 class Solution {
-typedef vector<int> p;
 private:
-void solve(p &candidates,int target,int index,vector<p> &ans,p &temp)
+void solve(vector<int> &candidates,int target,int index,vector<vector<int>> &ans,vector<int> &temp)
 {
     //base case
         if(target<0) return;
@@ -12,7 +11,7 @@ void solve(p &candidates,int target,int index,vector<p> &ans,p &temp)
         }
     
     for(int j=index;j<candidates.size();j++){
-        if(j > index && candidates[j] == candidates[j - 1]) continue; 
+        if(j > index && candidates[j] == candidates[j - 1]) continue; //cheak for duplicates
         
             temp.push_back(candidates[j]);
             solve(candidates,target-candidates[j],j+1,ans,temp);
@@ -26,8 +25,8 @@ void solve(p &candidates,int target,int index,vector<p> &ans,p &temp)
 
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-       vector<p> ans;
-        p temp;
+       vector<vector<int>> ans;
+        vector<int> temp;
         sort(begin(candidates),end(candidates));
         solve(candidates,target,0,ans,temp);
         return ans;    
