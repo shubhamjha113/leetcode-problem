8a7f4c6bb7be0af911e18dcec13f09cd55a86d90
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-02-27 01:25:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (43.18%) - LeetSync

--- 390-elimination-game/elimination-game.cpp
+++ 390-elimination-game/elimination-game.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int lastRemaining(int n) {
-        return n == 1 ? 1 : 2 * (1 + n / 2 - lastRemaining(n / 2));
+        bool left = true;
+        int head =1,step=1,remain=n;
+        while(remain>1){
+            if(left || (remain&1)){
+                head += step;
+            }
+            step *=2;
+            remain /=2;
+            left = !left;
+        }
+        return head;
     }
 };
