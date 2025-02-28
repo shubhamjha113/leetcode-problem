6c209d6f8b657e1e61c892913bfbbc52ee780e3c
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-02-28 22:58:39 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (51.69%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -6,7 +6,7 @@ public:
         stack<int> st;
 
         for(int i=nums2.size()-1;i>=0;i--){
-            while(!st.empty() && nums2[i]>st.top()){
+            while(!st.empty() && nums2[i] >= st.top()){
                 st.pop();
             }
             greater[nums2[i]]= st.empty()?-1: st.top();
