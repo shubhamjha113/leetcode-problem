6d8ab298731c8077bcc927863ac46ce7da4c5999
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-02-28 16:38:26 +0530

    Time: 2 ms (74.92%) | Memory: 10 MB (92.73%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,27 +1,7 @@
 class Solution {
 public:
-bool cheak(int n,int i,vector<string> &board,int j){
-    //upper left diagonal cheak
-    int row=i,col=j;
-    while(row>-1 && col >-1){
-        if(board[row][col]=='Q'){
-            return 0;
-        }
-        row--,col--;
-    }
 
-    //upper right
-    row =i,col=j;
-    while(row>-1 && col <n){
-        if(board[row][col]=='Q'){
-            return 0;
-        }
-        row--,col++;
-    }
-    return 1;
-
-}
-void find(int row,int n,vector<vector<string>> &ans,vector<string> &board,vector<int> &column){
+void find(int row,int n,vector<vector<string>> &ans,vector<string> &board,vector<int> &column, vector<bool> &leftDig, vector<bool> &rightDig){
     //base case 
     if(row == n){
         ans.push_back(board);
@@ -29,12 +9,16 @@ void find(int row,int n,vector<vector<string>> &ans,vector<string> &board,vector
     }
 
     for(int j=0;j<n;j++){
-        if(column[j] == 0 && cheak(n,row,board,j)){
+        if(column[j] == 0 && leftDig[n-1+row-j]==0 && rightDig[row+j]==0){
             column[j]=1;
             board[row][j]='Q';
-            find(row+1,n,ans,board,column);
+            leftDig[n-1+row-j]= 1;
+            rightDig[row+j]=1;
+            find(row+1,n,ans,board,column,leftDig,rightDig);
             column[j]=0;
             board[row][j]='.';
+            leftDig[n-1+row-j]= 0;
+            rightDig[row+j]=0;
         }
     }
 }
@@ -48,9 +32,10 @@ void find(int row,int n,vector<vector<string>> &ans,vector<string> &board,vector
                 board[i].push_back('.');
             }
         }
-
+        vector<bool> leftDig(2*n-1,0);
+        vector<bool> rightDig(2*n-1,0);
         vector<int> column(n,0);
-        find(0,n,ans,board,column);
+        find(0,n,ans,board,column,leftDig,rightDig);
         return ans;
     }
 };
