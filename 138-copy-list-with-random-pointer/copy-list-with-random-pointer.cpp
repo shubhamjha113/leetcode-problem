4ced1dba55cb5a732e1c0449d48989d928db6531
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-03-08 09:30:39 +0530

    Time: 3 ms (91.40%) | Memory: 14.9 MB (86.92%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -18,6 +18,7 @@ class Solution {
 public:
 
     Node* copyRandomList(Node* head) {
+        if(!head) return  NULL;
         Node* headcopy = new Node(0);
         Node* tailcopy=headcopy,*temp = head;
         while(temp){
@@ -28,24 +29,36 @@ public:
         tailcopy = headcopy;
         headcopy = headcopy->next;
         delete tailcopy;
-
         tailcopy = headcopy;
         temp = head;
-        unordered_map<Node*,Node*> m;
-        while(temp){
-            m[temp] = tailcopy;
-            temp = temp->next;
-            tailcopy = tailcopy->next;
+        //insert them in LL 
+        Node *curr1 = head,*curr2 = headcopy;
+        Node* front1,*front2;
+        while(curr1){
+            front1 = curr1->next;
+            front2 = curr2->next;
+            curr1->next = curr2;
+            curr2->next = front1;
+            curr1 = front1;
+            curr2 = front2;
         }
-        
-        tailcopy = headcopy;
-        temp = head;
+        //assign random pointer
+        curr1 = head;
+        while(curr1){
+            curr2 = curr1->next;
+            if(curr1->random){
+                curr2->random = curr1->random->next;
+            }
+            curr1 = curr2->next;
+        }
 
-        while(temp){
-            tailcopy->random = m[temp->random];
-            tailcopy = tailcopy->next;
-            temp = temp->next;
+        //disconnect them 
+        curr1 = head;
+        while(curr1->next){
+            front1 = curr1->next;
+            curr1->next = front1->next;
+            curr1 = front1;
         }
-        return headcopy;
+       return headcopy;
     }
 };
