b4b352bf63c78362667e3a932f96bc8b5d9e8d1e
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-03-18 20:37:35 +0530

    Time: 3 ms (27.94%) | Memory: 9.7 MB (58.32%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
@@ -9,9 +9,9 @@ public:
         }
         while(k--){
             sum=0;
-            for(int i=0;i<B.size();i++){
-             digit = B[i]-'0';
-            sum +=digit;
+            for(char c:B){
+             
+            sum +=c-'0';
             }
            B = to_string(sum);
         }
