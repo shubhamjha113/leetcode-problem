b0091b2b61b9597830fba2ada4affe99e68e9489
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-03-22 23:25:48 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (83.42%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,60 +1,8 @@
 class Solution {
 public:
-
-    void lpsfind(vector<int>& lps,string s) {
-       
-        int suff = 1,pre =0;
-        while(suff < s.size()){
-            //if matched 
-            if(s[suff] == s[pre]){
-                lps[suff] = pre +1;
-                suff++;
-                pre++;
-            }
-            //not match
-            else{
-                if(pre ==0){//agar wo zero index ko point kr rha h 
-                    lps[suff] =0;
-                    suff++;
-                }
-                else{
-                    pre = lps[pre-1];
-                }
-            }
-        }
-    }
-
-
-    int present(string temp, string goal) {
-        vector<int> lps(goal.size(),0);
-        lpsfind(lps,goal);
-        int first = 0,second = 0;
-        while(first < temp.size() && second < goal.size()){
-            if(temp[first] == goal[second]){
-                first++;
-                second++;
-            }
-            else{
-                if(second == 0){
-                    first++;
-                }
-                else{
-                    second = lps[second-1];
-                }
-            }
-        }
-        if(second == goal.size()){
-            return 1;
-        }
-        return 0;
-    }
-
-
     bool rotateString(string s, string goal) {
         if(s.size() != goal.size()) return false;
-        if(present(s+s,goal)==1){
-            return true;
-        }
-        return false;
+
+        return (s+s).find(goal) != string::npos;
     }
 };
