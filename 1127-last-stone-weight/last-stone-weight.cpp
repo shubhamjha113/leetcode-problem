d5bb08b32b8e163969f51037729c1e859f334232
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-03-30 00:48:58 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (9.52%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> p(stones.begin(),stones.end());
+
+        while( p.size()>1){
+            int x = p.top();
+            p.pop();
+            int y = p.top();
+            p.pop();
+            if(x!=y){
+                p.push(abs(x-y));
+            }
+        }
+        return (p.empty()) ? 0 : p.top();
+    }
+};
