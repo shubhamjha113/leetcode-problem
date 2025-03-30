2a58d038760e818579ac10d2a4e6321bd401624b
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-03-31 00:55:22 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (50.73%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -10,48 +10,32 @@
  */
 class Solution {
 public:
-
-    ListNode* merge(ListNode* head1,ListNode* head2){
-      ListNode* head = new ListNode(0);
-      ListNode* tail = head;
-      while(head1 && head2){
-          if(head1->val <= head2->val){
-              tail->next = head1;
-              head1 = head1->next;
-              tail = tail->next;
-              tail->next = NULL;
-          }
-          else{
-              tail->next = head2;
-              head2 = head2->next;
-              tail = tail->next;
-              tail->next = NULL;
-          }
+    class Compare{
+      public:
+      bool operator()(ListNode* a,ListNode* b){
+          return a->val > b->val;
       }
-      if(head1){
-          tail->next = head1;
-      }
-      else{
-          tail->next = head2;
-      }
-      return head->next;
-    }
-    
-    void mergesort(vector<ListNode*>& lists,int start,int end){
-        if(start>=end){
-            return;
-        }
-
-        int mid = start + (end-start)/2;
-        mergesort(lists,start,mid);
-        mergesort(lists,mid+1,end);
-        lists[start] = merge(lists[start],lists[mid+1]);
-    }
+  };
 
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.empty()) return NULL;
-        mergesort(lists,0,lists.size()-1);
-       
-        return lists[0];
+         priority_queue<ListNode* , vector<ListNode*>,Compare> p;
+
+         for(auto it:lists){
+            if(it) p.push(it);
+         }
+        
+        ListNode* root = new ListNode(0);
+        ListNode* tail = root;
+        ListNode* temp;
+        while(!p.empty()){
+            temp = p.top();
+            p.pop();
+            tail->next = temp;
+            tail = tail->next;
+            if(temp->next){
+                p.push(temp->next);
+            }
+        }
+        return root->next;
     }
 };
