92de68435b7952b2da153a7b07823f459cff9522
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-04-03 17:04:25 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (99.14%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
@@ -3,19 +3,18 @@ public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
         int n =deck.size();
         vector<int> ans(n,0);
-        queue<int> q;
-        for(int i=0;i<n;i++){
-            q.push(i);
-        }
         sort(begin(deck),end(deck));
-        for(int i=0;i<n;i++){
-            int index = q.front();
-            q.pop();
-            ans[index] = deck[i];
-            if(!q.empty()){
-                q.push(q.front());
-                q.pop();
+        int i=0,j=0;
+        bool skip = false;
+        while(i<n){
+            if(!ans[j]){
+                if(!skip){
+                    ans[j] = deck[i];
+                    i++;
+                }
+                skip = !skip;
             }
+            j = (j+1)%n;
         }
         return ans;
     }
