c89a9f8c4778eec10bceb4675b3654522e45de8d
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-04-04 18:58:25 +0530

    Time: 5 ms (92.56%) | Memory: 37.1 MB (46.75%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -3,20 +3,36 @@ public:
     int equalPairs(vector<vector<int>>& grid) {
         int n = grid.size();
         int count=0;
-        for(int r=0;r<n;r++){
-            for(int c=0;c<n;c++){
-                bool exist = true;
-                for(int i=0;i<n;i++){
-                    if(grid[r][i] != grid[i][c]){
-                        exist = false;
-                        break;
-                    }
-                }
-                if(exist == true){
-                    count++;
-                }
-            }
+        // BBRUTE FORCE APPROACH   0(N^3) TIME COMPLEXITY
+        // for(int r=0;r<n;r++){
+        //     for(int c=0;c<n;c++){
+        //         bool exist = true;
+        //         for(int i=0;i<n;i++){
+        //             if(grid[r][i] != grid[i][c]){
+        //                 exist = false;
+        //                 break;
+        //             }
+        //         }
+        //         if(exist == true){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+
+        map<vector<int> ,int> mp;
+        for(int r = 0;r<n;r++){
+            mp[grid[r]]++;
         }
+
+        for(int c=0;c<n;c++){
+            vector<int> temp;
+            for(int r=0;r<n;r++){
+               temp.push_back(grid[r][c]); 
+            }
+            count += mp[temp];
+        }
+
         return count;
     }
 };
