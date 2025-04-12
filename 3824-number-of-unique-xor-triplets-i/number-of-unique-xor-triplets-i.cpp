c2ee4c40ff55f19d2f6367928675e363045b3bd2
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-04-12 22:33:00 +0530

    Time: 0 ms (100.00%) | Memory: 198.7 MB (82.89%) - LeetSync

--- 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
-        int n = nums.size();
-        if(n==1 || n==2) return n;
-        int p = ceil(log2(n+1));
-        return pow(2, p);
+         int n = nums.size();
+
+        if(n < 3) return n;
+
+        // Find the Most Significant Bit
+        int cnt = 0;
+        int temp = n;
+        while(temp > 0){
+            temp >>= 1;
+            cnt++;
+        }
+        
+        return pow(2, cnt);
     }
 };
