1feec3c216cdfe15818930120b6c6ddbb183b30d
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-04-14 18:07:49 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (31.55%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -12,14 +12,14 @@ public:
     for (char ch : s) {
         freq[ch].second++;
     }
-        auto lamda = [&](pair<char,int> &p1,pair<char,int> &p2){
-            return p1.second>p2.second;
-        };
+    auto lamda = [&](pair<char,int> &p1,pair<char,int> &p2){
+        return p1.second>p2.second;
+    };
 
 
-        sort(begin(freq),end(freq),lamda);
+    sort(begin(freq),end(freq),lamda);
 
-        for (auto &p : freq) {
+    for (auto &p : freq) {
         if (p.second > 0) {
             ans += string(p.second, p.first);
         }
