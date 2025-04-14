e981200d863f601f0b66c34fbdba110acb52b199
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-04-14 22:13:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (30.99%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-    int noOfVowels(string a){
-        int count=0;
-        for(int i=0;i<a.size();i++){
-            char ch = tolower(a[i]);
-            if(ch=='a' || ch=='e'||ch=='i'|| ch=='o'||ch=='u'){
-                count++; 
-            }
-        }
-        return count;
-    }
-
+    //approach 1
+    // int noOfVowels(string a){
+    //     int count=0;
+    //     for(int i=0;i<a.size();i++){
+    //         char ch = tolower(a[i]);
+    //         if(ch=='a' || ch=='e'||ch=='i'|| ch=='o'||ch=='u'){
+    //             count++; 
+    //         }
+    //     }
+    //     return count;
+    // }
+    // bool halvesAreAlike(string s) {
+    //     int n = s.size();
+    //     string s1 =s.substr(0,n/2);
+    //     string s2 =s.substr(n/2);
+    //     return noOfVowels(s1)==noOfVowels(s2);
+    // }
     bool halvesAreAlike(string s) {
-        int n = s.size();
-        string s1 =s.substr(0,n/2);
-        string s2 =s.substr(n/2);
-        return noOfVowels(s1)==noOfVowels(s2);
+        unordered_set<char> vowels = {'a','e','i','o','u','A','E','I','O','U'};
+        int mid = s.size()/2;
+        int ans=0;
+        for(int i=0;i<mid;i++){
+            if(vowels.count(s[i])) ans++;
+            if(vowels.count(s[i+mid])) ans--;
+        }
+        return ans==0;
     }
 };
