43a6c023ee448da7b1605365f19384d84230b9f8
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-04-14 19:35:45 +0530

    Time: 10 ms (15.38%) | Memory: 22.2 MB (54.79%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
-
+    /*Compare characters from the left and right ends of the string.
+    If they match, continue moving inward.
+    If a mismatch occurs, we have one chance to remove a character 
+    (either from the left or the right)*/
 
     bool ispalindrome(string &s,int left,int right){
         while(left < right){
