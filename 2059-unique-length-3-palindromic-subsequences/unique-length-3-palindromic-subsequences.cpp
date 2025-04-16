6e2e5d6cc340f461a94f13836a7db5fac34a2ef5
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-04-16 11:56:37 +0530

    Time: 135 ms (77.21%) | Memory: 15.8 MB (81.62%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        unordered_set<char> letters;
-        for(char &ch:s){
-            letters.insert(ch);
-        } 
+        vector<pair<int,int>> index(26,{-1,-1});
+        for(int i=0;i<s.size();i++){
+            
+            if(index[s[i]-'a'].first==-1){
+                index[s[i]-'a'].first=i;
+            }
+            index[s[i]-'a'].second = i;
+        }
         int ans =0;
-        for(char letter:letters) {
-            int left = -1,right=-1;
-            for(int i=0;i<s.size();i++){
-                if(s[i]==letter){
-                    if(left==-1){
-                        left=i;
-                    }
-                    right = i;
-                }
+        for(int i=0;i<26;i++) {
+            int left = index[i].first;
+            int right= index[i].second;
+            if(left==-1){
+                continue;
             }
 
             unordered_set<char> st;
