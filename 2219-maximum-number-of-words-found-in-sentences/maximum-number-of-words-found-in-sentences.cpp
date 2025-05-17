fe0b3e65dced24355c88d0156ab3ad2511e59474
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-05-17 10:57:57 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (55.22%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.cpp
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-    int count(string s){
-        int a=1;
-        for(int i=0;i<s.size();i++){
-            if(s[i]==' '){
-                a++;
-            }
-        }
-        return a;
-    }
-
     int mostWordsFound(vector<string>& sentences) {
-        int maxi =0;
-        for(int i=0;i<sentences.size();i++){
-            int size=count(sentences[i]);
-            maxi = max(maxi,size);
+        int ans=0;
+        for(string &s:sentences){
+            int count=1;
+            for(char &ch:s){
+                if(ch==' ') count++;
+            }
+            ans = max(ans,count);
         }
-        return maxi;
+        return ans;
     }
 };
