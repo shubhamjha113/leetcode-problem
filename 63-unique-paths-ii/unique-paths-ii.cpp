a913ecd8b9872fea30667243f2b2544e240a3be6
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-06-02 10:52:34 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (53.74%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -10,7 +10,6 @@ public:
             return 0;
         }
         vector<vector<int>> dp(m,vector<int>(n,0));
-        
        for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(i==0 && j==0){
