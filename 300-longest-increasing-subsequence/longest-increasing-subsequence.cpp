019cc546e47fb67ce768eb81166b527f9e289341
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-06-09 15:45:27 +0530

    Time: 482 ms (23.09%) | Memory: 296.2 MB (29.40%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    int f(int ind,int prev_ind,vector<int>&nums,vector<vector<int>> &dp){
-        if(ind==nums.size()) return 0;
-        if(dp[ind][prev_ind+1] != -1) return dp[ind][prev_ind+1];
-        int len = 0+f(ind+1,prev_ind,nums,dp);//not Take
-        if(prev_ind==-1 || nums[ind]>nums[prev_ind]){
-            len = max(len,1+f(ind+1,ind,nums,dp));
-        }
-        return dp[ind][prev_ind+1] = len;
-    }
-
-
-    int lengthOfLIS(vector<int>& nums) {
+     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return f(0,-1,nums,dp);
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int ind = n-1;ind>=0;ind--){
+            for(int prev_ind = n-1;prev_ind>=-1;prev_ind--){
+                int len = 0+dp[ind+1][prev_ind+1];//not Take
+                if(prev_ind==-1 || nums[ind]>nums[prev_ind]){
+                    len = max(len,1+dp[ind+1][ind+1]);
+                }
+                dp[ind][prev_ind+1] = len;
+            }
+        }
+
+        return dp[0][0];
     }
 };
