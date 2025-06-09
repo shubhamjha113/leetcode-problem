81a039c7a7eb72921feb6f9749673f6be5a0b911
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-06-09 17:16:46 +0530

    Time: 154 ms (55.87%) | Memory: 139.9 MB (53.15%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-   int f(int ind,int tran_no,vector<int>&prices,vector<vector<int>> &dp){
+   int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        if(ind==n || tran_no==4) return 0;
-        if(dp[ind][tran_no]!= -1) return dp[ind][tran_no];
-        if(tran_no%2==0){
-            return dp[ind][tran_no] = max(-prices[ind]+f(ind+1,tran_no+1,prices,dp),f(ind+1,tran_no,prices,dp));
+        vector<vector<int>> dp(n+1,vector<int>(5,0));
+        for(int ind = n-1;ind>=0;ind--){
+            for(int tran_no = 3;tran_no>=0;tran_no--){
+                if(tran_no%2==0)
+                     dp[ind][tran_no] = max(-prices[ind]+dp[ind+1][tran_no+1],dp[ind+1][tran_no]);
+                else
+                     dp[ind][tran_no] = max(prices[ind]+dp[ind+1][tran_no+1],dp[ind+1][tran_no]);
+            }
         }
-        return dp[ind][tran_no] = max(prices[ind]+f(ind+1,tran_no+1,prices,dp),f(ind+1,tran_no,prices,dp));
-    }
-
-    int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<int>> dp(n,vector<int>(4,-1));
-        return f(0,0,prices,dp);
+        return dp[0][0];
     }
 };
