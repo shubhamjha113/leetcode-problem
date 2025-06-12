ede6de0d07de76bc81ed0e79608caf6b37bf311a
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-06-12 09:54:55 +0530

    Time: 33 ms (54.91%) | Memory: 22.1 MB (48.81%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,44 +1,25 @@
 class Solution {
 public:
-int f(int ind,vector<int> &coins,  int target,vector<vector<int>>& dp){
-	//base case
-	if(ind == 0){
-		if(target% coins[0]==0) return target / coins[0];
-		
-		return 1e9;
-	}
-	if(dp[ind][target]!=-1) return dp[ind][target];
-
-
-	int notTake = f(ind-1,coins,target,dp);
-	int take = INT_MAX;
-	if(coins[ind] <= target) take = 1+ f(ind,coins,target - coins[ind],dp);//index doesnot reduce here 
-
-	return dp[ind][target] =  min(take,notTake);
-
-}
-
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        vector<int>prev(amount+1,0),curr(amount+1,0);
         for(int tar = 0;tar<= amount;tar++){
             if(tar%coins[0]==0){
-                dp[0][tar] = tar/coins[0];
+                prev[tar] = tar/coins[0];
             }
             else
-                dp[0][tar]= 1e9;
+                prev[tar]= 1e9;
         }
         for(int ind = 1;ind<n;ind++){
             for(int tar = 0;tar<= amount;tar++){
-                int notTake = dp[ind-1][tar];
+                int notTake = prev[tar];
                 int take = INT_MAX;
-                if(coins[ind] <= tar) take = 1+ dp[ind][tar - coins[ind]];
-                 dp[ind][tar] =  min(take,notTake);
+                if(coins[ind] <= tar) take = 1+ curr[tar - coins[ind]];
+                 curr[tar] =  min(take,notTake);
             }
+            prev = curr;
         }
-
-
-	    int ans = dp[n - 1][amount];
+	    int ans = prev[amount];
         return (ans >= 1e9) ? -1 : ans;
     }
 };
