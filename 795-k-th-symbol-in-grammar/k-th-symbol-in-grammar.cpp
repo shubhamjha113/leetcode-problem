c0bfbfb0a9f58c05e337a1e7e047076475cba7ba
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-06-13 01:53:26 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (15.83%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
@@ -3,11 +3,6 @@ public:
     int kthGrammar(int n, int k) {
         if(n==1 && k==1) return 0;
         int len = pow(2,n-1);
-        
-
-
-         if(k<= len/2) return kthGrammar(n-1,k);
-         else return !kthGrammar(n-1,k-len/2);
-        return 0;
+        return (k<= len/2) ? kthGrammar(n-1,k) : !kthGrammar(n-1,k-len/2);
     }
 };
