0be06c28f9d75d9a79582f4a82fe74def4f0a904
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-06-24 11:10:23 +0530

    Time: 252 ms (21.49%) | Memory: 403.6 MB (35.53%) - LeetSync

--- 1880-largest-merge-of-two-strings/largest-merge-of-two-strings.cpp
+++ 1880-largest-merge-of-two-strings/largest-merge-of-two-strings.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     string largestMerge(string word1, string word2) {
-        int i=0,j=0;
-        string ans="";
-        while(i<word1.size() && j<word2.size()){
-            if(word1.substr(i)>word2.substr(j)){
-                ans+=word1[i];
-                i++;
-            }
-            else{
-                ans += word2[j];
-                j++;
+        int n = word1.size();
+        int m = word2.size();
+        int i = 0, j = 0;
+        string ans = "";
+        while(i < n && j < m) {
+            if(word1.substr(i) > word2.substr(j)){
+                ans+=word1[i++];
+            }else{
+                ans+=word2[j++];
             }
         }
-        while(i<word1.size()) ans+=word1[i++];
-        while(j<word2.size()) ans+=word2[j++];
-
+        while (i < n) ans+=word1[i++];
+        while (j < m) ans+=word2[j++];
         return ans;
     }
 };
