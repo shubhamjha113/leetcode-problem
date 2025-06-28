e34b15ec424efad3c3da7a9fc905f0f276dc624a
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-06-28 18:40:17 +0530

    Time: 174 ms (27.39%) | Memory: 117.8 MB (30.07%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-typedef pair<int,int> P;
+
     int longestSubarray(vector<int>& nums, int limit) {
-        priority_queue<P,vector<P>> maxPQ;
-        priority_queue<P,vector<P>,greater<P>> minPQ;
+        multiset<int> st;
         int l=0,r=0,len = 0;
         while(r<nums.size()){
-            maxPQ.push({nums[r],r});
-            minPQ.push({nums[r],r});
-            while(maxPQ.top().first - minPQ.top().first > limit){
-                l = min(maxPQ.top().second,minPQ.top().second) + 1;
-                while(maxPQ.top().second <l) maxPQ.pop();
-                while(minPQ.top().second <l) minPQ.pop();
+            st.insert(nums[r]);
+            while(*st.rbegin() - *st.begin() > limit){
+                st.erase(st.find(nums[l]));
+                l++;
             }
             len = max(len,r-l+1);
             r++;
