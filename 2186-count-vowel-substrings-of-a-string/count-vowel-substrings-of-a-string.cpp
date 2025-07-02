18c6e922ba8342e19419bb9eac73da9f7d933330
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-07-02 17:33:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (64.37%) - LeetSync

---
+++ 2186-count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    bool isVowel(char ch){
+        if(ch=='a' ||  ch=='e' || ch=='i' ||ch=='o' || ch=='u') return true;
+        return false;
+    }
+
+    int countVowelSubstrings(string word) {
+         unordered_map<char,int> mp;
+        int consonent =0;
+        int n = word.size();
+        vector<int> nextConsonent(n);
+        int ind = n;
+        nextConsonent[n-1] = ind;
+        for(int i=n-1;i>=0;i--){
+            nextConsonent[i] = ind;
+            if(!isVowel(word[i])){
+                ind = i;
+            }
+        }
+
+        int i=0,j=0;
+        int ans = 0;
+        while(j<word.size()){
+            char ch = word[j];
+            if(isVowel(ch)){
+                mp[ch]++;
+            }
+            else{
+                consonent++;
+            }
+
+            while(consonent !=0){
+                char ch = word[i];
+                if(isVowel(ch)){
+                    mp[ch]--;
+                    if(mp[ch]==0) mp.erase(ch);
+                }
+                else consonent--;
+
+                i++;
+            }
+            ind =0;
+            while(mp.size()==5 && consonent==0){
+                ans += nextConsonent[j]-j;
+                char ch = word[i];
+                if(isVowel(ch)){
+                    mp[ch]--;
+                    if(mp[ch]==0) mp.erase(ch);
+                }
+                else consonent--;
+                i++;
+            }
+            j++;
+        }
+        return ans;
+    }
+};
