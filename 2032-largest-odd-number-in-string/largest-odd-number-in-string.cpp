cfa50d471e4b9166215070d7c88606cb2386801e
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-07-03 18:47:14 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (64.12%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -4,9 +4,7 @@ public:
         string ans = "";
         int index =-1;
         int n = num.size();
-        if(num[n-1] %2 == 1){
-            return num;
-        }
+        
         for(int i=n-1;i>=0;i--){
             if(num[i]%2==1){
                 index = i;
