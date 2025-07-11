aeae64a6c433aaae462286129dc819da56c253d4
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-07-11 17:21:03 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (58.04%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -4,14 +4,12 @@ public:
         vector<int> ans;
         int n = nums.size();
         if(n<2) return nums;
-        int first = nums[0],second= nums[0] , fc= 0, sc=0;  // fc- first count   sc- second count
+        int first = NULL,second= NULL , fc= 0, sc=0;  // fc- first count   sc- second count
         for(int i=0;i<n;i++){
-            if( first == nums[i]){ 
-                first = nums[i];
+            if( nums[i]==first){ 
                 fc++;
             }
-            else if ( second == nums[i]){
-                second = nums[i];
+            else if( nums[i]==second){ 
                 sc++;
             }
             else if(fc == 0){
@@ -27,14 +25,11 @@ public:
                 sc--;
             }
         }
-        if(first == second){
-            ans.push_back(first);
-            return ans;
-        }
+        
         fc=0,sc=0;
         for(int i=0;i<n;i++){
-            if(first == nums[i]) fc++;
-            if(second == nums[i]) sc++;
+            if( nums[i]==first) fc++;
+            else if(nums[i] ==second) sc++;
         }
         if(fc > n/3){
             ans.push_back(first);
