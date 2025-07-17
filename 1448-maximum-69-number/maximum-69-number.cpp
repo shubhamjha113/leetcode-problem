db88371daaf37d33bbbdefabc8c566c9ce6472bc
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-07-17 13:00:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (13.29%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        string number = to_string(num);
-        for(int i=0;i<number.size();i++){
-            if(number[i]!= '9'){
-                number[i]='9';
-                break;
+        int placevalue = 0,placevalueSix = -1;
+        int temp = num;
+        while(temp>0){
+            int rem = temp%10;
+            if(rem ==6){
+                placevalueSix = placevalue;
             }
+            temp /=10;
+            placevalue++;
         }
-        return stoi(number);
+        if(placevalueSix == -1) return num;
+        return num + 3*pow(10,placevalueSix);
     }
 };
