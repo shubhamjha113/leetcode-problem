c346bf38da54ad545bbc5c555de44301b7af55cf
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-07-19 06:21:29 +0530

    Time: 23 ms (88.65%) | Memory: 57.1 MB (78.62%) - LeetSync

--- 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.cpp
+++ 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.cpp
@@ -5,7 +5,7 @@ public:
         sort(piles.begin(),piles.end());
         int n = piles.size();
         int b =0,a = n-1,m=n-2;
-        while(b<m && b<n && m>=0){
+        while(b<m){
             ans += piles[m];
             a-=2,m-=2,b+=1;
         }
