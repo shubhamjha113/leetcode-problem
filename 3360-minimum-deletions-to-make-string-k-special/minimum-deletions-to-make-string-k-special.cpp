a9b7b796daa7af2de6f7761496ab160a5f414051
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-07-21 11:41:00 +0530

    Time: 8 ms (58.85%) | Memory: 19.3 MB (82.06%) - LeetSync

---
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumDeletions(string word, int k) {
+        vector<int> freq(26,0);
+        for(char &ch:word){
+            freq[ch-'a']++;
+        }
+        int n = word.size(),res=n;
+        for(int i=0;i<26;i++){
+            int del =0;
+            for(int j=0;j<26;j++){
+                if(i==j) continue;
+
+                if(freq[j] < freq[i]){
+                    del += freq[j];
+                }
+                else if(abs(freq[j]-freq[i]) > k){
+                    del += abs(freq[j]-freq[i] -k);
+                }
+            }
+            res = min(res,del);
+        }
+        return res;
+    }
+};
