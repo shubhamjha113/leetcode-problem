88788e8c784e866910279a6210bf0d47439db906
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-08-05 17:56:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (27.74%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,42 +1,28 @@
 class Solution {
 public:
-    // Brute force using recurssion 
-
-    // bool fn(string s, int index,int count){
-    //     if(count<0) return false;
-
-    //     if(index==s.size()) return count==0;
-
-    //     if(s[index]=='('){
-    //         return fn(s,index+1,count+1);
-    //     }
-    //     if(s[index]==')'){
-    //         return fn(s,index+1,count-1);
-    //     }
-    //     return fn(s,index+1,count+1) || fn(s,index+1,count-1) || fn(s,index+1,count);
-    // }
     bool checkValidString(string s) {
-        if(s.size()==1){
+        int n = s.size();
+        if(n==1){
             if(s[0]=='*') return true;
             else return false;
         }
-        int min=0,max=0;
-        for(int i=0;i<s.size();i++){
+        
+        int min = 0,max = 0;
+        for(int i=0;i<n;i++){
             if(s[i]=='('){
-                min += 1;
-                max +=1;
-            }
-            else if(s[i]==')'){
-                min -=1;
-                max -=1;
-            }
-            else{
-                min -= 1;
+                min +=1;
                 max += 1;
             }
-            if(min<0) min =0;
-
-            if(max<0) return false;
+            else if(s[i]==')'){
+                min -= 1;
+                max -= 1;
+            }
+            else{
+                min -=1;
+                max +=1;
+            }
+            if(min <0) min = 0;
+             if(max<0) return false;
         }
         return min==0;
     }
