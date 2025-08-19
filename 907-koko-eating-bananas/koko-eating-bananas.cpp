9db780b784693588132f7d1d536d71966e99aa95
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-08-20 00:11:36 +0530

    Time: 11 ms (40.49%) | Memory: 23 MB (42.21%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,31 +1,28 @@
+
 class Solution {
 public:
-    bool isPos(vector<int>& piles, int h,int n , int mid) {
+    bool isPos(vector<int>& piles, int h, int mid) {
         int sum = 0;  
-        for (int i=0;i<n;i++) {
-           
-            sum += (piles[i] + mid - 1) / mid; 
-            if (sum > h) return false;
+        for (int &x:piles) {
+            sum += x/mid; 
+            if(x%mid !=0) sum++; 
         }
-        return true;
+        return sum <= h;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int n=piles.size();
         int s=1;
          
         int e = *max_element(piles.begin(),piles.end());
-        int mid=s + (e-s)/2;
-        int ans  = -1;
-        while(s<=e){
-            if(isPos(piles,h,n,mid)){
-                ans=mid;
-                e=mid-1;
+       
+        while(s<e){
+            int mid=s + (e-s)/2;
+            if(isPos(piles,h,mid)){
+                e=mid;
             }
             else{
                 s=mid+1;
             }
-            mid = s + (e-s)/2;
         }
-        return ans;
+        return s;
     }
 };
