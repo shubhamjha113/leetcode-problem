259b51bd1a9bd7cc594eb108295e1f93895e0d7a
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-08-31 00:35:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (57.21%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -33,3 +33,18 @@ public:
         return head;
     }
 };
+
+//two pointer use krke v ho jayega 
+/*
+    if(!head || !head->next){
+            return head;
+        }
+        ListNode* p = head,*q=head->next;
+        while(true){
+            swap(p->val,q->val);
+            if(!(q->next && q->next->next))break;
+            p = p->next->next;
+            q = q->next->next;
+        }
+        return head;
+*/
