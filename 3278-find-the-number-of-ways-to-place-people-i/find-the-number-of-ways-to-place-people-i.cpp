292ec3629a015fbe69b38bbe32bc50725052c8c6
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-09-03 19:09:05 +0530

    Time: 4 ms (72.46%) | Memory: 32.7 MB (21.74%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -3,26 +3,26 @@ public:
     int numberOfPairs(vector<vector<int>>& points) {
         int ans = 0;
         int n  = points.size();
+        auto lambda = [](vector<int>&p1,vector<int> &p2){
+            if(p1[0]==p2[0]){
+                return p1[1]>p2[1];
+            }
+            return p1[0]<p2[0];
+        };
+        sort(points.begin(),points.end(),lambda);
+        
         for(int i=0;i<n;i++){
             int x1 = points[i][0];
             int y1 = points[i][1];
-            
-            for(int j=0;j<n;j++){
-                if(i==j) continue;
+
+            int maxY = INT_MIN; // reset for each i 
+            for(int j=i+1;j<n;j++){
                 int x2 = points[j][0];
                 int y2 = points[j][1];
-                if(x1<=x2 && y1>=y2){
-                    bool inside = false;
-                    for(int k=0;k<n;k++){
-                        if(i==k || j==k) continue;
-                        int x3 = points[k][0];
-                        int y3 = points[k][1];
-                        if(x3>=x1 && x3<= x2 && y3>=y2 && y3<=y1){
-                            inside = true;
-                            break;
-                        }
-                    }
-                    if(!inside) ans++;
+                if(y2>y1) continue;
+                if(y2>maxY){
+                    ans++;
+                    maxY = y2;
                 }
             }
         }
