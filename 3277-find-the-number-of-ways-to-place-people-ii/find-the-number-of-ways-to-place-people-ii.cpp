e2eb2b08fb5fb56070e7835faaa7671e6daf5c91
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-09-03 19:11:51 +0530

    Time: 102 ms (97.87%) | Memory: 41.3 MB (19.15%) - LeetSync

---
+++ 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        int ans = 0;
+        int n  = points.size();
+        auto lambda = [](vector<int> &p1 , vector<int> &p2){
+            if(p1[0]==p2[0]){
+                return p1[1]>p2[1];
+            }
+            return p1[0]<p2[0];
+        };
+        sort(points.begin(),points.end(),lambda);
+        for(int i=0;i<n;i++){
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+            int maxY = INT_MIN;
+            for(int j=i+1;j<n;j++){
+                
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+                if(y2>y1) continue;
+                if(y2>maxY){
+                    maxY = y2;
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
