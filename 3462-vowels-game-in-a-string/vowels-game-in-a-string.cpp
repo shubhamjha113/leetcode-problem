4476a866e9365b67e248448bff34c20a81f82fae
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-09-12 10:00:16 +0530

    Time: 24 ms (5.60%) | Memory: 19.2 MB (25.75%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        string vowel = "aeiou";
+        int count = 0;
+        for(int i=0;i<s.size();i++){
+            if(vowel.find(s[i]) != string::npos){
+                count++;
+            }
+        }
+        if(count==0) return false;
+        if(count & 1) return true;
+        return true;
+    }
+};
