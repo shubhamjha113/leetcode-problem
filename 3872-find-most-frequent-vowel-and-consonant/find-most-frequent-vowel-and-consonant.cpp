d1e35bd430a05d400d34261e589f4fc319d837b6
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-09-13 08:41:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (81.58%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int maxFreqSum(string s) {
-        vector<int> vowel(26,0),consonent(26,0);
+        vector<int> count(26,0);
+        for(int i=0;i<s.size();i++){
+            count[s[i]-'a']++;
+        }
+        int vow=0,con=0;
         for(int i=0;i<s.size();i++){
             if(string("aeiou").find(s[i]) != string::npos){
-                vowel[s[i]-'a']++;
+                vow = max(vow,count[s[i]-'a']);
             }
             else{
-                consonent[s[i]-'a']++;
+                con = max(con,count[s[i]-'a']);
             }
         }
-        int max1 = *max_element(begin(vowel),end(vowel));
-        int max2 = *max_element(begin(consonent),end(consonent));
-        return max1 + max2;
+        return vow + con;
     }
 };
