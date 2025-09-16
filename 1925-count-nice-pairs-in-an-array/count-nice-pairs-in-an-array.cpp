eb381651db435d34391984e78000136fb7056e15
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-09-16 19:37:53 +0530

    Time: 72 ms (7.85%) | Memory: 63.2 MB (12.88%) - LeetSync

--- 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.cpp
+++ 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.cpp
@@ -9,7 +9,7 @@ public:
 
     int countNicePairs(vector<int>& nums) {
         int n = nums.size();
-        if(n==1) return 0;
+        //if(n==1) return 0;
         for(int i=0;i<n;i++){
             nums[i] = nums[i] - fRev(nums[i]);
         }
@@ -19,6 +19,6 @@ public:
             ans = (ans + mp[nums[i]])%mod;
             mp[nums[i]]++;
         }
-        return ans%mod;
+        return ans;
     }
 };
